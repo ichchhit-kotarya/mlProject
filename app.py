0142e9061691fcf641ec2a0c1ee45e5d6f0293e0
ichchhit kotarya <ichchhitkotaryage@gmail.com>
2025-04-27 20:32:55 +0530

    Model Tranier

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from mlproject.components.data_ingestion import DataIngestion
 from mlproject.components.data_ingestion import DataIngestionConfig
 from mlproject.components.data_transformation import DataTransformationConfig,DataTransformation
 import sys
+from mlproject.components.model_tranier import ModelTrainerConfig,ModelTrainer
 
 
 if __name__ =="__main__":
@@ -16,8 +17,12 @@ if __name__ =="__main__":
 
         #data_transformation_config=DataTransformationConfig()
         data_transformation=DataTransformation()
-        data_transformation.initiate_data_transormation(train_data_path,test_data_path)
+        train_arr,test_arr,_=data_transformation.initiate_data_transormation(train_data_path,test_data_path)
 
+
+        #Model Training
+        model_trainer=ModelTrainer()
+        print(model_trainer.initiate_model_trainer(train_arr,test_arr))
 
     except Exception as e:
         logging.info("Custom Exception")
--- src/mlproject/utils.py
+++ src/mlproject/utils.py
@@ -1,9 +1,11 @@
 import os
 import sys
-from mlproject.exception import CustomException
-from mlproject.logger import logging
+from src.mlproject.exception import CustomException
+from src.mlproject.logger import logging
 import pandas as pd
 from dotenv import load_dotenv
+from sklearn.model_selection import GridSearchCV
+from sklearn.metrics import r2_score
 import pymysql
 
 import pickle
@@ -14,7 +16,9 @@ load_dotenv()
 host=os.getenv("host")
 user=os.getenv("user")
 password=os.getenv("password")
-db=os.getenv("db")
+db=os.getenv('db')
+
+
 
 def read_sql_data():
     logging.info("Reading SQL database started")
@@ -23,17 +27,19 @@ def read_sql_data():
             host=host,
             user=user,
             password=password,
-            db=db,
+            db=db
         )
-        logging.info("Connection Established", mydb)
-        df=pd.read_sql_query("Select * from students",mydb)
+        logging.info("Connection Established",mydb)
+        df=pd.read_sql_query('Select * from students',mydb)
         print(df.head())
 
         return df
 
+
+
     except Exception as ex:
         raise CustomException(ex)
-
+    
 def save_object(file_path, obj):
     try:
         dir_path = os.path.dirname(file_path)
@@ -42,5 +48,37 @@ def save_object(file_path, obj):
 
         with open(file_path, "wb") as file_obj:
             pickle.dump(obj, file_obj)
+
     except Exception as e:
-        raise CustomException(e,sys)
+        raise CustomException(e, sys)
+
+def evaluate_models(X_train, y_train,X_test,y_test,models,param):
+    try:
+        report = {}
+
+        for i in range(len(list(models))):
+            model = list(models.values())[i]
+            para=param[list(models.keys())[i]]
+
+            gs = GridSearchCV(model,para,cv=3)
+            gs.fit(X_train,y_train)
+
+            model.set_params(**gs.best_params_)
+            model.fit(X_train,y_train)
+
+            #model.fit(X_train, y_train)  # Train model
+
+            y_train_pred = model.predict(X_train)
+
+            y_test_pred = model.predict(X_test)
+
+            train_model_score = r2_score(y_train, y_train_pred)
+
+            test_model_score = r2_score(y_test, y_test_pred)
+
+            report[list(models.keys())[i]] = test_model_score
+
+        return report
+
+    except Exception as e:
+        raise CustomException(e, sys)
